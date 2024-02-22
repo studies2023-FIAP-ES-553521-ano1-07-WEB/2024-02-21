d181e92d938cb08276e685a1ba24de5bcbd7ec15
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-21 22:37:09 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function mudar() {
+    //DECLARANDO AS VARIÁVEIS
+    let novo = document.getElementById("idTexto")
+
+    //CHAMANDO O TÍTULO
+    document.getElementById("titulo")
+    document.getElementById("titulo").innerHTML = novo.value;
+}
